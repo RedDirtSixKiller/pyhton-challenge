67541c12049e3726e36a8ee80987a2342eb8b140
RedDirtSixKiller <adam@ajpconline.com>
2019-07-02 17:04:21 -0700

    I think I finished for real this time

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,10 @@
 import csv
 import os
 
+def formatCurr(val):
+    currency_val = str("${:0,.2f}".format(val).replace("$-","-$"))
+    return currency_val
+
 path = os.getcwd()
 bank_csv = os.path.join(path,"budget_data.csv")
 total_months = 0
@@ -25,19 +29,21 @@ with open(bank_csv, newline='') as budget_data:
         total_diff_profit += diff_profit[0]
         
         if diff_profit[0]>great_month[1]:
-            great_month = [row[0],diff_profit[0]] 
+            great_month = [row[0], diff_profit[0]] 
         elif diff_profit[0]<least_month[1]:
             least_month = [row[0], diff_profit[0]]
         prev_line = row
     avg_profit_chng = round(total_diff_profit/(total_months-1),2)
-    print(total_months)
-    print(profit)
-    print(great_month)
-    print(least_month)
-    print(avg_profit_chng) 
+    print("Total Months: " + str(total_months))
+    print("Total Profit: " + str(formatCurr(profit)))
+    print("Avg Change: " + str(formatCurr(avg_profit_chng))) 
+    print("Greatest Month: " + str([great_month[0], formatCurr(great_month[1])]))
+    print("Least Month: " + str([least_month[0], formatCurr(least_month[1])]))
+    
 #trying to write out file better than hard coded
 titles = ["Total Months", "Profit", "Average Change", "Greatest Change", "Least Change"]
-values = [total_months, profit, avg_profit_chng, great_month, least_month]
+values = [formatCurr(total_months), formatCurr(profit), formatCurr(avg_profit_chng),
+         [least_month[0], formatCurr(least_month[1])], [least_month[0], formatCurr(least_month[1])]]
 output = zip(titles,values)
 #print(list(output))
 #Is there some object I can write to that can be use w/ print too?
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,22 +4,7 @@ import os
 path = os.getcwd()
 ele_csv = os.path.join(path,"election_data.csv")
 
-def unique_vals(list1): 
-  
-    # intilize a null list 
-    unique_list = [] 
-      
-    # traverse for all elements 
-    for x in list1: 
-        # check if exists in unique_list or not 
-        if x not in unique_list: 
-            unique_list.append(x) 
-    # print list 
-   # for x in unique_list: 
-    #    print(x), 
 
-#identify unique candidates
-#create a 2 part var w/ cands names and votes
 with open(ele_csv, newline='') as ele_data:
     csv_reader = csv.reader(ele_data, delimiter=",")
     csv_header = next(csv_reader)
@@ -27,5 +12,42 @@ with open(ele_csv, newline='') as ele_data:
 
     for row in csv_reader:
         cands.append(row[2])
-    unique_cands = unique_vals(cands)
+    
 
+cand_counts = {}
+total_votes = len(cands)
+for c in cands:
+    try:
+        cand_counts[c] = cand_counts[c] + 1
+    except KeyError:
+        cand_counts[c] = 1
+pbar = "-------------------------"
+max_per = 0
+output_file = os.path.join("output.csv")
+#Still need to output to file
+#open the output file
+with open(output_file, "w", newline="") as datafile:
+    writer=csv.writer(datafile)
+    #write header
+    writer.writerow(["Election Results"])
+    print("Election Results")
+    writer.writerow([pbar])
+    print([pbar])
+    writer.writerow(["Total Votes: " + str(total_votes)])
+    print("Total Votes: " + str(total_votes))
+    writer.writerow([pbar])
+    print(pbar)
+    for c in cand_counts:
+        per_votes = round(cand_counts[c]/total_votes,6)
+        if per_votes > max_per:
+            max_per = per_votes
+            winner = c
+        writer.writerow([c+ ": " + "percent: " + str('{:.3%}'.format(per_votes)) + " (" + str(cand_counts[c]) +")"])
+        print(c+ ": " + "percent: " + str('{:.3%}'.format(per_votes)) + " (" + str(cand_counts[c]) +")" )
+
+    writer.writerow([pbar])
+    print(pbar)
+    writer.writerow(["Winner: "+winner])
+    print("Winner: "+winner)
+    writer.writerow([pbar])
+    print(pbar)
